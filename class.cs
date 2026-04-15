408cc79c76d4bbca496dd28b9d6efdf9eadf3979
Tiago ramos <tiago.ramos@overlabs.com>
2026-04-15 14:02:41 -0300

    alteração de nova classe

---
+++ class.cs
@@ -0,0 +1,58 @@
+class{
+    pessoa = observar
+        less
+        more
+
+        1. more complex systems
+
+        integração - logs aws - kubernetes
+
+        logs em formatos diferentes - ?????
+
+        complexo + mais logs não estruturados
+
+        main font da observabilidade das empresas
+
+        95 dos devs é com base no logs
+
+        ------------------------------------------
+
+        lancamento - streams - getdata - menage them
+
+        grandes empresas - strams apoia
+
+        integration sera substituido pelo streams
+
+        usar os dois ao mesmo tempo - isso funciona mais para instrumentação
+
+        ingerir varias fontes - volume crescendo - log sensivel?
+        1 tera 2 tera, alerta, juntar o elastic com ferramentas de automação/n8n
+
+        keep - especializada em automação - tech preview with 9.3
+
+        templates - teve um alerta faz isso ......
+
+        o que de importante esta acontecendo nos meus logs?
+
+        workflows roadmap - observabilite
+
+        construtor visual - 
+
+        workflow é como se fosse um n8n
+
+        demo - trade a ações - monkey - 
+
+        superdemo - instrupt -
+
+        agentic rca for observabilite
+
+        agentes do elastic - preciso fazer rca
+        mcp - protocolo que vai padronizar os dados de como 
+        criar/padronizar pedido para ia 
+
+        bom do agente chat que ja verifica serviços existentes e 
+        usa para pedidos feitos, como por exemplo listar indices 
+
+        
+
+}
